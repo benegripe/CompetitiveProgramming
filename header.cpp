cacd9d78687f5bfe77eb1eb8c6030ec1c843f0c7
fabio lima <fabiolima39@gmail.com>
2022-09-07 16:18:00 -0300

    simple header to use in contest

---
+++ header.cpp
@@ -0,0 +1,16 @@
+#include <utility>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+//memset(memo, -1, sizeof memo);
+//memset(arr, 0, sizeof arr);
+
+const int INF = 1e9;
+const int LLINF = 4e18;
+const double EPS = 1e-9;
